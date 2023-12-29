587137743e3dac68801168ece8710125258bf6aa
Mohd Musaiyab <mohdmusaiyab123@gmail.com>
2023-12-29 17:09:48 +0530

    Pages Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import Home from "./pages/Home";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
 const App = () => {
   return (
-    <div className='text-red-400'>
-      App
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/sign-in" element={<SignIn></SignIn>}></Route>
+        <Route path="/sign-up" element={<SignOut></SignOut>}></Route>
+        <Route path="/profile" element={<Profile></Profile>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
