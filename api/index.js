9d717769b8403b80cd0b62588df9e9f7633bd59d
Mohd Musaiyab <mohdmusaiyab123@gmail.com>
2023-12-30 15:59:58 +0530

    DB connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to Databse");
+  })
+  .catch((err) => {
+    console.log("Connection Failed");
+    console.log(err);
+  });
+const app = express();
 
-const app=express();
-
-app.listen(3000,()=>{
-    console.log("Server is Listening on port 3000")
-})
+app.listen(3000, () => {
+  console.log("Server is Listening on port 3000");
+});
